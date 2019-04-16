e5aecb7d412b68062d2c54a182ff7afd10c3a2b8
Pau Gargallo <pau.gargallo@gmail.com>
2019-04-16 21:39:03 +0200

    chore: use yaml's save_load

--- opensfm/config.py
+++ opensfm/config.py
@@ -149,7 +149,7 @@ submodel_images_relpath_template: "submodels/submodel_%04d/images"   # Template 
 
 def default_config():
     """Return default configuration"""
-    return yaml.load(default_config_yaml)
+    return yaml.safe_load(default_config_yaml)
 
 
 def load_config(filepath):
@@ -158,7 +158,7 @@ def load_config(filepath):
 
     if os.path.isfile(filepath):
         with open(filepath) as fin:
-            new_config = yaml.load(fin)
+            new_config = yaml.safe_load(fin)
         if new_config:
             for k, v in new_config.items():
                 config[k] = v
